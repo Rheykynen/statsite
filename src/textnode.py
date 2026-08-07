d1f4b347c66cde343ac6bc854bd0e712195d1fa8
Rheykynen <e.heine210@gmail.com>
2026-08-07 15:28:59 +0200

    added test py and sh

--- src/textnode.py
+++ src/textnode.py
@@ -1,21 +1,27 @@
 from enum import Enum
 
 class TextType(Enum):
-      PLAIN = "text"
-      BOLD = "**Bold text**"
-      ITALIC = "_Italic text_"
-      CODE = "'Code text`"
-      LINKS = "[anchor text](url)"
-      IMAGES = "![alt text](url)"
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
 
 class TextNode():   
    def __init__(self, text, text_type, url=None):
       self.text = text
-      self.text_type = TextType[text_type]
+      self.text_type = text_type
       self.url = url
 
    def __eq__(self, other):
-       return self == other
+    if not isinstance(other, TextNode): # Wenn es nicht gleiche Typen sind, kann ich sie nicht vergleichen
+       return False
+    return ( # es soll jedes Element miteinander vergleichen werden
+            self.text_type == other.text_type
+            and self.text == other.text
+            and self.url == other.url
+        )
 
    def __repr__(self):
-       return (f"{self.text}, {self.text_type.value}, {self.url}")
+    return (f"{self.text}, {self.text_type.value}, {self.url}")
