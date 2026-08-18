eafc759ecb04f41e058bc168707c6c70ce87d880
Rheykynen <e.heine210@gmail.com>
2026-08-18 17:32:16 +0200

    lesson progression for single length delimiter

--- src/delimiter.py
+++ src/delimiter.py
@@ -10,17 +10,38 @@ def split_nodes_delimiter(
     for old_node in old_nodes:
         if old_node.text_type != text_type.TEXT:
             new_nodes.append(old_node)
+            continue
         else:
-            character = old_node.text
-            hits = []
-            for i in range(len(character)):
-                if character[i] == delimiter:
-                    hits.append(i)
-            if len(hits) % 2 != 0:
-                raise Exception(f"Invalid Syntax, no closing delimiter in Text")
+            start_idx = None
+            end_idx = None
 
-            delimited = character[hits[0]+1: hits[1]]
-            print(character.split(delimited))
+            node_text = old_node.text
+            for i in range(len(node_text)):
+                if node_text[i] == delimiter and start_idx is None:
+                    start_idx = i
+                    continue
+                if node_text[i] == delimiter and end_idx is None:
+                    end_idx = i
+            if end_idx is None:
+                raise Exception("Invalid syntax, no closing delimiter in text.")
+
+            left_side = node_text[:start_idx]
+            delimited_text = node_text[start_idx + 1:end_idx]
+            right_side = node_text[end_idx + 1:]
+
+            if left_side:
+                new_nodes.append(
+                        TextNode(f"{left_side}", old_node.text_type),
+                )
+            new_nodes.append(TextNode(f"{delimited_text}", text_type))
+            if right_side:
+                new_nodes.append(TextNode(f"{right_side}", old_node.text_type))
+
+            print(new_nodes)
+
+
+
+
 
 
 
@@ -29,3 +50,18 @@ def split_nodes_delimiter(
 
     return new_nodes
 
+"""    for old_node in old_nodes:
+        if old_node.text_type != text_type.TEXT:
+            new_nodes.append(old_node)
+        else:
+            character = old_node.text
+            hits = []
+            for i in range(len(character)):
+                if character[i] == delimiter:
+                    hits.append(i)
+            if len(hits) % 2 != 0:
+                raise Exception(f"Invalid Syntax, no closing delimiter in Text")
+            deli_start = hits[0]
+            deli_end = hits[1]
+            delimited = character[deli_start + 1: deli_end]
+            print(character.split(delimited))"""
--- src/test_delimiter.py
+++ src/test_delimiter.py
@@ -8,10 +8,34 @@ class TestDelimiter(unittest.TestCase):
         node = TextNode("This is text with a `code block` word", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
         self.assertEqual(new_nodes, [
-    TextNode("This is text with a ", TextType.TEXT),
-    TextNode("code block", TextType.CODE),
-    TextNode(" word", TextType.TEXT),
-])
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ])
+
+    def test_split_nodes_code_in_front(self):
+        node = TextNode("`Code block` comes first, the rest afterwards.", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(new_nodes, [
+            TextNode("Code block", TextType.CODE),
+            TextNode(" comes first, the rest afterwards.", TextType.TEXT),
+        ])
+
+    def test_bold_delimiter(self):
+        node = TextNode("This is text with a **bold** marker", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(new_nodes, [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" marker", TextType.TEXT),
+        ])
+
+    def test_dont_split_no_delimiter(self):
+        node = TextNode("This is text with links and no delimiter", TextType.LINK)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(new_nodes, [
+            TextNode("This is text with links and no delimiter", TextType.LINK),
+        ])
 
     def test_missing_delimiter(self):
         node = TextNode("This is text with a `code block word", TextType.TEXT)
